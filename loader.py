fab8db9429d52928f119ec6f7523a7ed1764e2c6
1stuzzy <132016132+1stuzzy@users.noreply.github.com>
2024-09-03 20:46:08 +0500

    Add files via upload

--- loader.py
+++ loader.py
@@ -3,7 +3,7 @@ from aiogram.types import ParseMode
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 
-bot = Bot('6697941790:AAGF17sNqO6Ejd7vD58gH-mF6HCBIL4woOk', parse_mode=ParseMode.HTML)
+bot = Bot('', parse_mode=ParseMode.HTML)
 dp = Dispatcher(bot, storage=MemoryStorage())
 channel = -1002189593207
 
